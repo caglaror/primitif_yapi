5d1e555af9f3c234144e59c64e9492c1fb3d773b
caglaror <caglaror@gmail.com>
2018-05-07 01:10:16 +0300

    Vendors etc.

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="tr" lang="tr">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+    <meta http-equiv="resource-type" content="document" />
+    <meta http-equiv="distribution" content="global" />
+    <meta http-equiv="window-target" content="_top" />
+    <meta http-equiv="imagetoolbar" content="no" />
+    <meta http-equiv="Expires" content="Mon, 26 Jul 2008 05:00:00 GMT" />
+    <meta http-equiv="pragma" content="no-cache" />
